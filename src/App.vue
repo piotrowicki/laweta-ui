73157f6bdee453315d26b8900028950be5b204d6
Piotr Nowicki <piotrowicki@gmail.com>
2021-01-10 21:18:49 +0100

    Fixes in layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <div id="app">
-    <div id="stripped-background" />
-    <b-nav>
-      <b-nav-item>
-        <router-link to="/offer">OFERTA</router-link>
-      </b-nav-item>
-      <b-nav-item>
-        <router-link to="/contact">KONTAKT</router-link>
-      </b-nav-item>
-    </b-nav>
-    <div>
-      <b-container fluid="md" class="laweta">
-        <img class="laweta-img" src="@/assets/laweta2.gif" />
-        <div class="align">
-          <span id="log24" class="blink-me">24h</span>
-          <span class="osobowe-busy-tir">OSOBOWE BUSY TIR</span>
-          <span class="pomoc-drogowa-ostroda">POMOC DROGOWA OSTRÓDA</span>
-          <span id="phone" class="blink-me">505 582 392</span>
-        </div>
+    <header>
+      <div id="stripped-background" />
+      <b-nav>
+        <b-nav-item>
+          <router-link to="/offer">OFERTA</router-link>
+        </b-nav-item>
+        <b-nav-item>
+          <router-link to="/contact">KONTAKT</router-link>
+        </b-nav-item>
+      </b-nav>
+    </header>
+
+    <b-container fluid="md" class="laweta">
+      <img class="laweta-img" src="@/assets/laweta2.gif" />
+      <div class="align">
+        <span id="log24" class="blink-me">24h</span>
+        <span class="osobowe-busy-tir">OSOBOWE BUSY TIR</span>
+        <span class="pomoc-drogowa-ostroda">POMOC DROGOWA OSTRÓDA</span>
+        <span id="phone" class="blink-me">505 582 392</span>
+      </div>
+    </b-container>
+    <main>
+      <b-container>
         <router-view />
       </b-container>
-      <footer></footer>
-    </div>
+    </main>
+    <footer></footer>
   </div>
 </template>
 
