048be1e94cc97dda1c0819e6f017ecde31eacef5
Piotr Nowicki <piotrowicki@gmail.com>
2021-02-06 21:08:11 +0100

    Add bootstrap icons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -9,8 +9,7 @@ Vue.config.productionTip = false
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(BootstrapVueIcons)
 
 new Vue({
   router,
