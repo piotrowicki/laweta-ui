c1e6d16cc8045bcbafb8e9b9c10c46f13f69343e
Piotr Nowicki <piotrowicki@gmail.com>
2021-05-03 18:30:03 +0200

    Add PrimeVue imports

--- src/main.js
+++ src/main.js
@@ -2,14 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import PrimeVue from 'primevue/config'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
 Vue.config.productionTip = false
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(PrimeVue)
 
 new Vue({
   router,
