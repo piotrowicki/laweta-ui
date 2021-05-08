5e05188a39e5c227efd0b0dca1ae1ee208bebc98
Piotr Nowicki <piotrowicki@gmail.com>
2021-05-08 13:30:57 +0200

    Add primeflex

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css';
 
 Vue.config.productionTip = false
 
